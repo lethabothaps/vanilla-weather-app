d17f189bb64ca67f5f0ae674f339c461c53a0189
lethabothaps <lethabo.lt21@gmail.com>
2023-08-15 23:23:36 +0200

    changed the url for the API call

--- src/app.js
+++ src/app.js
@@ -68,7 +68,7 @@ function diplayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(diplayForecast);
 }
 
